cad3eda88caea96d27ba7cc03231c1239a706646
Dmitry Korolev <dmitrii.korolev1@gmail.com>
2018-11-30 16:50:09 +0300

    merge

--- SP2016.Repository/IRepository.cs
+++ SP2016.Repository/IRepository.cs
@@ -3,36 +3,36 @@
     public interface IRepository<in TContext, TEntity> where TEntity : new()
     {
         /// <summary>
-        /// Add new entity
+        /// Add a new entity
         /// </summary>
-        /// <param name="context">Node which contains the repository</param>
+        /// <param name="context">Context</param>
         /// <param name="entity">Entity to be added</param>
         void Add(TContext context, TEntity entity);
 
         /// <summary>
-        /// Update an entity
+        /// Update an existing entity
         /// </summary>
-        /// <param name="context">Node which contains the repository</param>
+        /// <param name="context">Context</param>
         /// <param name="entity">Entity to be updated</param>
         void Update(TContext context, TEntity entity);
 
         /// <summary>
-        /// Get item by unique identificator.
+        /// Get item by identifier
         /// </summary>
-        /// <param name="context">Node which contains the repository</param>
-        /// <param name="id">Unique identificator</param>
+        /// <param name="context">Context</param>
+        /// <param name="id">identifier</param>
         TEntity GetEntityById(TContext context, int id);
 
         /// <summary>
-        /// Get collection of entities by query
+        /// Get entities by query
         /// </summary>
-        /// <param name="context">Node which contains the repository</param>
+        /// <param name="context">Context</param>
         /// <param name="expr">Query expression</param>
         /// <param name="rowLimit">Max number of entities to be returned</param>
         TEntity[] GetEntities(TContext context, object expr, uint rowLimit);
 
         /// <summary>
-        /// Get collection of all existing entities
+        /// Get all entities
         /// </summary>
         /// <param name="context">Node which contains the repository</param>
         /// <param name="recursive">Include entities from sub-repositories</param>
