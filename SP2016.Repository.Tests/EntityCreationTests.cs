cf8e77324c74d0f3aea7bc0bb2f1822e3032d009
Vlad Loputs <vloput@gmail.com>
2018-11-30 09:55:36 +0300

    Tests for CreateEntity and GetLastEntities were implemented.

---
+++ SP2016.Repository.Tests/EntityCreationTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.SharePoint;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection;
+
+namespace SP2016.Repository.Tests
+{
+    [TestClass]
+    public class EntityCreationTests : BaseRepoTest
+    {
+        [TestMethod]
+        public void CreateEntity_FromListItem_EntityFilledWithRightData()
+        {
+            Perform(web =>
+            {
+                try
+                {
+                    UsersRepository.Add(web, MockUsers.User1);
+                    var item = UsersRepository.GetEntitiesByTitle(web, MockUsers.User1.DisplayName)[0].ListItem;
+
+                    var entity = UsersRepository.CreateEntity(web, item);
+
+                    Assert.IsNotNull(entity);
+                    Assert.AreEqual(MockUsers.User1.DisplayName, entity.DisplayName);
+                }
+                finally
+                {
+                    UsersRepository.DeleteAll(web);
+                }
+            });
+        }
+    }
+}
---
+++ SP2016.Repository.Tests/GetLastEntitiesTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SP2016.Repository.Tests
+{
+    [TestClass]
+    public class GetLastEntitiesTests : BaseRepoTest
+    {
+        [TestMethod]
+        public void GetLastEntities_LastTwo_ReturnsTwoInProperOrder()
+        {
+            Perform(web => {
+                try
+                {
+                    UsersRepository.Add(web, MockUsers.User3);
+                    UsersRepository.Add(web, MockUsers.User4);
+                    UsersRepository.Add(web, MockUsers.User1);
+
+                    uint numberOfNeededUsers = 2;
+                    var lastCreatedUsers = UsersRepository.GetLastEntities(web, numberOfNeededUsers);
+
+                    Assert.AreEqual((int)numberOfNeededUsers, lastCreatedUsers.Length,
+                        $"Expected {numberOfNeededUsers} users to receive, but {lastCreatedUsers.Length} received");
+
+                    Assert.AreEqual(MockUsers.User1.DisplayName, lastCreatedUsers[0].DisplayName);
+                }
+                finally
+                {
+                    UsersRepository.DeleteAll(web);
+                }
+            });
+        }
+    }
+}
